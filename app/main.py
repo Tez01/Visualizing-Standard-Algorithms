2b5680fbd8470d2829f9f3418c41656436559e1f
Tez01 <tejpartap02@gmail.com>
2022-03-03 17:43:02 -0500

    Again Added functionality to render html by creating templates directory structure

--- app/main.py
+++ app/main.py
@@ -1,7 +1,12 @@
 from .routers import home
 from fastapi import FastAPI
+from fastapi.staticfiles import StaticFiles
 
 
 app =   FastAPI()
 
+# Used to handle static files. Mounts an independent application to handle static files
+app.mount("/static", StaticFiles(directory = "static"), name = "static")
+
+# Include created routes in the router folder to the main application.
 app.include_router(home.router) 
--- app/routers/home.py
+++ app/routers/home.py
@@ -1,5 +1,8 @@
-from fastapi import APIRouter, status
+from fastapi import APIRouter, status, Request
 from fastapi.responses import HTMLResponse
+from fastapi.templating import Jinja2Templates
+
+templates = Jinja2Templates(directory="templates")
 
 router = APIRouter(
         prefix = "/home",
@@ -7,16 +10,6 @@ router = APIRouter(
 )
 
 @router.get("", status_code= status.HTTP_200_OK, response_class = HTMLResponse)
-async def home():
-        return        """
-        <!DOCTYPE html>
-<html lang="en">
-        <head>
-                <title>Home</title>
-        </head>
-        <body>
-                <h1>This is the home page of my application</h1>
-        </body>
-</html>
-        """
+async def home(request: Request):
+        return templates.TemplateResponse("index.html", {"request": request})
 
