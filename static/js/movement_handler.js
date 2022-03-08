77cb10096efd46374aca474d18ba98a6d22cc8be
Tez01 <tejpartap02@gmail.com>
2022-03-07 19:19:19 -0500

    Add extend layout functionality to all html files

--- static/js/movement_handler.js
+++ static/js/movement_handler.js
@@ -6,7 +6,8 @@ document.addEventListener('DOMContentLoaded', function(){
         searchButton1.style.animationPlayState = 'paused';
 
         // Wait for button to be clicked
-        document.querySelector('#search-button').onclick = function(){
+        searchButton1.onclick = function(){
+                // ANimate from center to left if already paused
                 if(searchButton1.style.animationPlayState == 'paused'){
                         searchButton1.style.animationPlayState = 'running';
                 }
@@ -16,5 +17,29 @@ document.addEventListener('DOMContentLoaded', function(){
                         searchButton1.style.animationPlayState = 'paused';
                 }
 
+                makeButtons(this, 2);
+
+
         };
 });
+
+function makeButtons(sourceElement, numOfButtons){
+        // Get width and height of source element
+        let width = sourceElement.offsetWidth;
+        let height = sourceElement.offsetHeight;
+
+        //Make numOfArrows number of arrows by alternating between up and down
+        for( let i = 0; i < numOfButtons; i++ ){
+
+                makeArrow()
+        }
+}
+
+
+
+
+
+
+
+// Todo:
+// Make the button in center
