7ddac9929e2c3b75d66bd2ce6140535d89b81ea8
Tez01 <tejpartap02@gmail.com>
2022-03-15 19:10:09 -0400

    Code cleanup in styles.css

--- static/js/movement_handler.js
+++ static/js/movement_handler.js
@@ -1,36 +1,4 @@
-document.addEventListener('DOMContentLoaded', function(){
-        // Find search button
-        const searchButton1 = document.querySelector('#search-button');
-        
-        // Pause animation
-        searchButton1.style.animationPlayState = 'paused';
 
-        // Wait for button to be clicked
-        searchButton1.onclick = function(){
-                // ANimate from center to left if already paused
-                if(searchButton1.style.animationPlayState == 'paused'){
-                        searchButton1.style.animationPlayState = 'running';
-                }
-
-                // Otherwise pause the animation
-                else{
-                        searchButton1.style.animationPlayState = 'paused';
-                }
-
-                console.log( parseInt(window.getComputedStyle(searchButton1).animationDuration.split("s")[0]))
-                const buttonText = ["Search", "Sort", "Hash"]
-                setTimeout(()=>{
-                        makeButtons(buttonText, document.querySelector(".search-algorithm") );    
-                }, (parseInt(window.getComputedStyle(searchButton1).animationDuration.split("s")[0]) / 2) * 1000)
-                // Trigger creation of buttons when animation is at half
-                searchButton1.addEventListener('animationend',()=>{
-                        // make new buttons
-                
-                        
-                })
-
-        };
-});
 
 function makeButtons( buttonText, containerToAddButtons){
         // Create a container to save all buttons
@@ -39,18 +7,74 @@ function makeButtons( buttonText, containerToAddButtons){
 
         // Create buttons in the container based on number of text items in buttonText
         for(let button = 0; button < buttonText.length; button++){
-                buttonContainer.innerHTML += `<button type="button" class= "algorithm-type-button button">${buttonText[button]}</button>`
+                buttonContainer.innerHTML += `<button type="button" id= "algorithm-type-${button}" class= "button algorithm-type-button">${buttonText[button]}</button>`
         }
 
         // Add the element to the container passed in argument
         containerToAddButtons.append(buttonContainer);
 
+}document.addEventListener('DOMContentLoaded', function(){
+        // Find search button
+        const searchButton1 = document.querySelector('#search-button');
+        
+        // Pause animation
+        searchButton1.style.animationPlayState = 'paused';
+
+        // Wait for button to be clicked
+        searchButton1.onclick = function(){
+                startAnimation(searchButton1);
+
+                const buttonText = ["Search", "Sort", "Hash"]
+                async function f(){
+                        let promise = new  Promise((resolve, reject)=>{
+                                setTimeout(()=>{
+                                        // make new buttons
+                                        resolve(makeButtons(buttonText, document.querySelector(".search-algorithm") ));    
+                                }, (parseInt(window.getComputedStyle(searchButton1).animationDuration.split("s")[0]) / 2) * 1000) // Trigger creation of buttons when animation is at half
+
+                         })
+                         return promise;
+                }
+                f().then(()=>{
+                                        // Find search button
+                                const algorithmTypeButton= document.querySelector('#algorithm-type-0');
+                                console.log(algorithmTypeButton)
+                                if(algorithmTypeButton != null){
+                                        algorithmTypeButton.style.animationPlayState = 'paused';
+                                        algorithmTypeButton.onclick = function(){
+                                                
+                                                startAnimation(algorithmTypeButton);
+                                        }
+                                }
+                        }
+                )
+        };
+
+        
+        
+
+});
+
+
+
+/**
+ * Starts the animation for a particular element passed as argument.  Undefined if element doesnot have any
+ * animations
+ * 
+ * @param {object} element Element for which animation need to be turned on
+ * @return  Nothing
+ */
+function startAnimation(element){
+        
+        // Start animation if already paused
+        if(element.style.animationPlayState == 'paused'){
+                element.style.animationPlayState = 'running';
+        }
+        // Otherwise pause the animation
+        else{
+                element.style.animationPlayState = 'paused';
+        }
 }
-
-
-
-
-
 
 
 // Todo:
