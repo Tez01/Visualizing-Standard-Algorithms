2b3ff46b79e1122db1d77cdac1a892bbe4b116d0
Tez01 <tejpartap02@gmail.com>
2022-04-09 17:05:00 -0400

    Add animation to shrining of visualization area

--- static/js/visualize_handler.js
+++ static/js/visualize_handler.js
@@ -1,29 +1,29 @@
 document.addEventListener('DOMContentLoaded', function(){
-
+        let inputData = null;
+        let cleanedArray = null;
         document.querySelector('#vp-item3-2').onkeyup = function(event){ // vp-item3-2 is the input box
                 if(event.key === ","){        
                         clearDiv(document.querySelector('#vp-item2-2'));    // Clear the visualization area
                         //visualize the input in the textbox
-                        const visualizeDiv = createElem("div", "vp-item2-2-1", [], "")
+                        const visualizeDiv = createElem("div", "vp-item2-2-1", [], "", "visualizeDiv")  // This is dynamically created here,
+                                                                                 // So that previous display is cleared and can start displaying from fresh.
                         document.querySelector('#vp-item2-2').append(visualizeDiv);
-                        displayList(this.value.split(","), document.querySelector('#vp-item2-2-1'));  // passing an array of data points instead of string
-                }
-                
-                
+                        inputData = this.value.split(",")
+                        cleanedArray = cleanArray(inputData) // Clean the input array for any non-real number values.
+                        displayList(cleanedArray, document.querySelector('#vp-item2-2-1'));  // passing an array of data points instead of string
+                }         
         }
-
 
         // Visualize on clicking start
         document.querySelector('#vp-item4').onclick = function(){       // When start button is clicked
-                if(document.querySelector('#vp-item1').innerText === "Selection Sort"){  
-                        const inputData = document.querySelector('#vp-item3-2').value
-                        console.log(inputData)
-                        if(inputData !== []){
-                                const algorithm = new SelectionSort(inputData)
+                if(document.querySelector('#vp-item1').innerText === "Selection Sort"){ 
+                        if(inputData !== null){
+                                disableClick(this)
+                                const algorithm = new SelectionSort(cleanedArray)
                                 algorithm.visualize();
+                                console.log(this)
                         }
                 }
-                console.log(document.querySelector('#vp-item1').innerText)
         }
 })
 
@@ -39,10 +39,10 @@ document.addEventListener('DOMContentLoaded', function(){
  * @return  Nothing
  */
 function displayList(arrayData, DOMElement){
-        const cleanedArray = cleanArray(arrayData) // Clean the input array for any non-real number values.
-        const maxValue = getMaxInArray(cleanedArray);// Get max value in array
-        console.log(`Cleaned array: ${cleanedArray} , maxValue: ${maxValue}`);
-        cleanedArray.forEach(dataElement=>{
+       
+        const maxValue = getMaxInArray(arrayData);// Get max value in array
+        console.log(`Cleaned array: ${arrayData} , maxValue: ${maxValue}`);
+        arrayData.forEach(dataElement=>{
                 const element = createElem('div', "", ["rectangle"], `height : ${parseInt((dataElement/ maxValue) * 100)}%`)
                 DOMElement.append(element);
         });
@@ -74,7 +74,6 @@ function cleanArray(arrayData){
                 if(!isNaN(parseInt(element))){  // If element is not a NaN
                         newArray.push(Math.abs(parseInt(element)) )// Push the positive int portion to newArray
                 }
-                console.log(typeof NaN)
         });
         return newArray;
 }
@@ -98,11 +97,12 @@ function clearDiv(parent){
  * @param {string}  typeOfElement type of DOM element to be created. Eg 'div', 'p' etc.
  * @param {string}  id Id to assign to the newly created element.
  * @param {array}   classList An array of string containing all classes to add to element
- * @param {string}  styleString a string representing the style to add to the element
+ * @param {string}  styleString a string representing the style to add to the element *
+ * @param {string}  name a string for name of the element.
  * Assumes all parameters are valid, otherwise behaviour is undefined. Example for typeOfElement "z", behaviour is undefined.
  * 
  */
-function createElem(typeOfElement, id, classList, styleString){
+function createElem(typeOfElement, id, classList, styleString, name){
         const element = document.createElement(typeOfElement);
         // Add id
         element.setAttribute('id', id);
@@ -112,6 +112,8 @@ function createElem(typeOfElement, id, classList, styleString){
         });
         // add style
         element.style = styleString;
+        // add name
+        element.setAttribute('name', name)
 
         return element;
 
@@ -135,11 +137,15 @@ class SelectionSort extends Algorithm{
         constructor(arrayData){
                 super(arrayData, "Selection Sort")
         }
+        print(){
+                console.log(this);
+        }
 
         visualize(){
                 //makeTwoPortions
                 const visualizeContainer = document.querySelector("#vp-item2-2");
-                visualizeContainer.style.gridTemplateRows = "9fr 2fr"
+                document.documentElement.style.setProperty('--visualize-grid-template-row', "8fr 2fr");  // Set the grid row template to make shrinking effect
+                visualizeContainer.style.setProperty('animation-play-state', 'running') // Start animation for shrinking
                 // Appear single box below first box after green and assign it min value
                 // Make both red
                 // Go to end while checking minimum, if new minimum found make that bar red
@@ -151,3 +157,12 @@ class SelectionSort extends Algorithm{
 }
 
 
+/**
+ * Disable the element's click property 
+ * 
+ * @param {object} element Element for which click needs to be disabled
+ * @return  Nothing
+ */
+ function disableClick(element){
+        element.onclick = null;
+}
